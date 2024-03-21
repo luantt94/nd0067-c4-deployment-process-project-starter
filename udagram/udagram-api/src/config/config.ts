56c8e353122c78d2c54bff0808de619c48f9b183
luantt94.uit <luantt94.uit@gmail.com>
2024-03-21 12:38:09 +0700

    add elasticbeanstalk, create user with AdministratorAccess-AWSElasticBeanstalk instead of AWSElasticBeanstalkFullAccess

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
